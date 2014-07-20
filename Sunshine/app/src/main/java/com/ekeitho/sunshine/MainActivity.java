fa53636fdd69d38af6f57f360c28beb573a1f599
Keith Abdulla <email@ekeitho.com>
2014-07-20 12:04:42 -0700

    added some fake data into our fragment

--- Sunshine/app/src/main/java/com/ekeitho/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/ekeitho/sunshine/MainActivity.java
@@ -11,6 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,6 +28,7 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
+
 
 
     @Override
@@ -57,7 +61,21 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
+
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
             return rootView;
         }
     }
