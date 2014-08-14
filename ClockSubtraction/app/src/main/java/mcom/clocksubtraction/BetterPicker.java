c9d9433d947d223598cc2268246e03aaf0fdb2a1
Keith A <email@ekeitho.com>
2014-08-13 19:27:59 -0700

    update to new clock sub

--- ClockSubtraction/app/src/main/java/mcom/clocksubtraction/BetterPicker.java
+++
@@ -1,7 +0,0 @@
-package mcom.clocksubtraction;
-
-/**
- * Created by m652315 on 8/12/14.
- */
-public class BetterPicker {
-}
--- Sunshine/app/src/main/java/com/ekeitho/sunshine/FetchWeatherTask.java
+++
@@ -1,7 +0,0 @@
-package com.ekeitho.sunshine;
-
-/**
- * Created by m652315 on 8/1/14.
- */
-public class FetchWeatherTask {
-}
